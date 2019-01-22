73ec76686177acb62bb76604b9cbc2f0fd29459f
Michael Jeffries <mjeffries@pros.com>
2019-01-21 18:09:26 -0600

    Trim arrays questions

--- app/arrays.js
+++ app/arrays.js
@@ -1,16 +1,5 @@
 arraysAnswers = {
   /**
-   * Find the 0 based index of item in arr.
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @param {Number} item - A number to find in arr
-   * @returns {Number} The index of item in arr, or -1 if item is not in arr.
-   */
-  indexOf: function indexOf(arr, item) {
-    // Implement a function, that returns the 0 based index of an element in an array.
-  },
-
-  /**
    * Determine the sum of the items of arr.
    * 
    * @param {Number[]} arr - An array of numbers
@@ -32,72 +21,6 @@ arraysAnswers = {
   },
 
   /**
-   * Adds a number, item, to the end of an array, arr.
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @param {Number} item - A number to be appended to the end of arr
-   * @returns {Number[]} The array arr, with item appended.
-   */
-  append: function append(arr, item) {
-
-  },
-
-  /**
-   * Removes a number, item, from the end of an array, arr.
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @returns {Number[]} The array arr, with the last element removed..
-   */
-  truncate: function truncate(arr) {
-
-  },
-
-  /**
-   * Adds a number, item, to the beginning of an array arr.
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @param {Number} item - A number to append to the beginning of arr.
-   * @returns {Number[]} The array arr, with the first element item added
-   */
-  prepend: function prepend(arr, item) {
-
-  },
-
-
-  /**
-   * Removes the first element from an array
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @returns {Number[]} The array arr, with the first element item removed.
-   */
-  curtail: function curtail(arr) {
-
-  },
-
-  /**
-   * Combines the two arrays arr1 and arr2 together
-   * 
-   * @param {Number[]} arr1 - An array of numbers
-   * @param {Number[]} arr2 - An array of numbers
-   * @returns {Number[]} A new array, with elements from arr1 and arr2 in that order.
-   */
-  concat: function concat(arr1, arr2) {
-
-  },
-
-  /**
-   * Insert a number item into an array arr at the 0 based position index.
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @param {Number} item - A number to be inerted into arr
-   * @param {Number} index - A 0 based index into the array arr.
-   * @returns {Number[]} The array arr, with the number item inserted at position index.
-   */
-  insert: function insert(arr, item, index) {
-
-  },
-
-  /**
    * Counts the number of times a number item appears in an array arr.
    * 
    * @param {Number[]} arr - An array of numbers
@@ -115,16 +38,6 @@ arraysAnswers = {
    * @returns {Number[]} An array of numbers that appear in arr more than once.
    */
   duplicates: function duplicates(arr) {
-
-  },
-
-  /**
-   * Squares each number in an array arr. Example: square([1, 2, 3]) returns [1, 4, 9].
-   * 
-   * @param {Number[]} arr - An array of numbers
-   * @returns {Number[]} A new array of numbers that contains the elements of arr squared.
-   */
-  square: function square(arr) {
 
   },
 
