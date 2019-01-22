4637225fedae488608e943b1b79f81bd647674d5
Michael Jeffries <mjeffries@pros.com>
2019-01-21 18:17:43 -0600

    Remove nondeterminism from tests

--- tests/app/flowControl.js
+++ tests/app/flowControl.js
@@ -1,11 +1,5 @@
 describe('flow control', () => {
   it('you should be able to conditionally branch your code', () => {
-    let num = 0;
-
-    while (num % 3 === 0 || num % 5 === 0) {
-      num = Math.floor(Math.random() * 10) + 1;
-    }
-
     expect(flowControlAnswers.fizzBuzz()).not.to.be.ok;
     expect(flowControlAnswers.fizzBuzz('foo')).not.to.be.ok;
     expect(flowControlAnswers.fizzBuzz(2)).to.eql(2);
@@ -13,13 +7,10 @@ describe('flow control', () => {
 
     expect(flowControlAnswers.fizzBuzz(3)).to.eql('fizz');
     expect(flowControlAnswers.fizzBuzz(6)).to.eql('fizz');
-    expect(flowControlAnswers.fizzBuzz(num * 3)).to.eql('fizz');
 
     expect(flowControlAnswers.fizzBuzz(5)).to.eql('buzz');
     expect(flowControlAnswers.fizzBuzz(10)).to.eql('buzz');
-    expect(flowControlAnswers.fizzBuzz(num * 5)).to.eql('buzz');
 
     expect(flowControlAnswers.fizzBuzz(15)).to.eql('fizzbuzz');
-    expect(flowControlAnswers.fizzBuzz(num * 3 * 5)).to.eql('fizzbuzz');
   });
 });
