6bd9a0707f5b3fc76642e0f0b4a0d1a0807064c4
Michael Jeffries <mjeffries@pros.com>
2019-01-21 18:11:47 -0600

    Trim async questions

--- app/async.js
+++ app/async.js
@@ -1,16 +1,5 @@
 asyncAnswers = {
   /**
-   * Asynchronously returns a value via a promise. Example:
-   * async('anyValue').then((result) => { return result === 'anyValue';});
-   * 
-   * @param value - Any value
-   * @returns {then: function} A promise like object containing a then property.
-   */
-  async: function async(value) {
-
-  },
-
-  /**
    * Creates a promise that resolves with the data returned from an ajax call to the url url.
    * You may use jquery, XMLHttpRequest, or fetch.
    * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
--- tests/app/async.js
+++ tests/app/async.js
@@ -1,29 +1,4 @@
 describe('async behavior', () => {
-  it('you should understand how to use promises to handle asynchronicity', (done) => {
-    let flag = false;
-    let finished = 0;
-    const total = 2;
-
-    function finish(_done) {
-      finished += 1;
-      if (finished === total) { _done(); }
-    }
-
-    asyncAnswers.async(true).then((result) => {
-      flag = result;
-      expect(flag).to.eql(true);
-      finish(done);
-    });
-
-    asyncAnswers.async('success').then((result) => {
-      flag = result;
-      expect(flag).to.eql('success');
-      finish(done);
-    });
-
-    expect(flag).to.eql(false);
-  });
-
   it('you should be able to retrieve data from the server and return a sorted array of names', (done) => {
     const url = '/data/testdata.json';
 
